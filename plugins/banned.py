ada7454f50a57ad4bb8c8c895205b39424b09c0f
surya808493 <surajkumar75419696@gmail.com>
2025-07-13 10:52:03 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -39,7 +39,7 @@ async def ban_reply(bot, message):
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
     buttons = [[
-        InlineKeyboardButton('Support', url='https://t.me/Deendayal_Hindi_Movies')
+        InlineKeyboardButton('Support', url='t.me/Deendayal_dhakad_Contact')
     ]]
     reply_markup=InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)
