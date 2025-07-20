d2920ff582dc2912bf182a595013ad7fc4cddea2
Nirajk0077 <indok666@gmail.com>
2025-07-20 06:56:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,7 +112,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "https://previous-juliette-rolexfile-ec0d4964.koyeb.app/")
+URL = environ.get("FQDN", "https://crude-elnora-rolexfile-68db11e7.koyeb.app/")
 
 # Commands
 admin_cmds = [
